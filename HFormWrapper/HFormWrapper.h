004f4be25555a75c16a96242b2d04b1f576d7d1b
Hunt3rDe <github@hunt3r.de>
2018-02-11 21:58:32 +0100

    Initial Commit

---
+++ HFormWrapper/HFormWrapper.h
@@ -0,0 +1,19 @@
+//
+//  HFormWrapper.h
+//  HFormWrapper
+//
+//  Created by Hunt3rDe on 11.02.18.
+//  Copyright © 2018 Hunt3rDe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFormWrapper.
+FOUNDATION_EXPORT double HFormWrapperVersionNumber;
+
+//! Project version string for HFormWrapper.
+FOUNDATION_EXPORT const unsigned char HFormWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFormWrapper/PublicHeader.h>
+
+
